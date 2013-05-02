1e5da91f0d252d8a2c878e8e144c91f10a77780c
Aditya Phatak <aditya.phatak@yahoo.co.in>
2013-05-02 15:49:50 +0530

    python code for restart

--- scripts/restart.py
+++ scripts/restart.py
@@ -2,10 +2,6 @@
 PYTHON_EXECUTABLE = "python3"  # don't touch this, it's modified by debian rules
 
 import os
-
 def run():
-    command = "/usr/bin/sudo /sbin/shutdown -r now"
-    import subprocess
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-
-
+ 
+    os.system("gnome-session-quit --reboot --no-prompt ")
